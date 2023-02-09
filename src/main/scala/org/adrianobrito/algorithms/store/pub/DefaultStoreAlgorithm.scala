6e2f46ffe1b64135a086b395f28d2d276826f54d
Adriano Brito <adriano.brito@zalando.de>
2023-02-09 02:25:54 +0100

    small: fix broken tests

--- src/main/scala/org/adrianobrito/algorithms/store/pub/DefaultStoreAlgorithm.scala
+++ src/main/scala/org/adrianobrito/algorithms/store/pub/DefaultStoreAlgorithm.scala
@@ -1,6 +1,11 @@
 package org.adrianobrito.algorithms.store.pub
 
 import cats.effect.kernel.Async
-import org.adrianobrito.algorithms.store.StoreAlgorithm
+import org.adrianobrito.algorithms.store.pub.StoreAlgorithm
+import org.adrianobrito.model.StoreRequest
 
-class DefaultStoreAlgorithm[F[_]: Async] extends StoreAlgorithm[F] {}
+class DefaultStoreAlgorithm[F[_]: Async] extends StoreAlgorithm[F] {
+
+  override def store(request: StoreRequest): F[Unit] = ???
+
+}
--- src/main/scala/org/adrianobrito/algorithms/store/pub/StoreAlgorithm.scala
+++ src/main/scala/org/adrianobrito/algorithms/store/pub/StoreAlgorithm.scala
@@ -2,6 +2,6 @@ package org.adrianobrito.algorithms.store.pub
 
 import org.adrianobrito.model.StoreRequest
 
-trait StoreAlgorithm {
+trait StoreAlgorithm[F[_]] {
   def store(request: StoreRequest): F[Unit]
 }
