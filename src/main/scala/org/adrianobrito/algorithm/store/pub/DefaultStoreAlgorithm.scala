a50516b83ebc5b118f99ef1a8b2beb5a1dc1fb2d
Adriano Brito <adriano.brito@auto1.com>
2023-02-15 00:20:51 +0100

    Add Minor Fix (#7)
    Co-authored-by: Adriano Brito <adriano.brito@zalando.de>

--- src/main/scala/org/adrianobrito/algorithm/store/pub/DefaultStoreAlgorithm.scala
+++ src/main/scala/org/adrianobrito/algorithm/store/pub/DefaultStoreAlgorithm.scala
@@ -25,19 +25,26 @@ class DefaultStoreAlgorithm[F[_]: Async](
   private def splitResponsesByStatus(responses: List[StoreResponse]): (List[StoreResponse], List[StoreResponse]) =
     (responses.filter(_.isLeft), responses.filter(_.isRight))
 
+  private def toContacts(responses: F[List[StoreResponse]]): F[Set[Contact]] =
+    responses.flatMap(r => Async[F].pure(r.flatMap(_.toList).toSet))
+
   override def store(key: NodeId, data: Array[Byte])(iterations: Int = maxIterations): F[Set[Contact]] = {
     val responses: F[(List[StoreResponse], List[StoreResponse])] =
       sendStoreRequests(key, data).flatMap { r =>
         Async[F].delay(splitResponsesByStatus(r))
       }
-    val failedResponses     = responses._1F
-    val successfulResponses = responses._2F
-    val successfulResponsesContacts =
-      successfulResponses.flatMap(responses => Async[F].pure(responses.flatMap(_.toList).toSet))
+    val failedResponses             = responses._1F
+    val successfulResponses         = responses._2F
+    val successfulResponsesContacts = toContacts(successfulResponses)
+    val failedResponsesContacts     = toContacts(failedResponses)
+
+    successfulResponses.flatMap(responses => Async[F].pure(responses.flatMap(_.toList).toSet))
 
     failedResponses.flatMap { fResponses =>
       if (fResponses.size > threshold.value && iterations > 0) {
         store(key, data)(iterations - 1)
+      } else if (iterations == 0) {
+        failedResponsesContacts
       } else {
         successfulResponsesContacts
       }
