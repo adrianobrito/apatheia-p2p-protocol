60a7de664e23cd945cf1257bc80a336feb360f28
Adriano Brito <adriano.brito@zalando.de>
2023-05-22 20:23:07 +0200

    clean shit

--- src/main/scala/org/apatheia/validation/ValidationOps.scala
+++
@@ -1,7 +0,0 @@
-package org.apatheia.validation
-
-object ValidationOps {
-  implicit class ValidationOpsClass[A: Valid](a: A) {
-    def validate(implicit valid: Valid[A]) = valid.validate(a)
-  }
-}
