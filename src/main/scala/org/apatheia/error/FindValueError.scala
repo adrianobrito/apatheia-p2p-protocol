706cb5bcbb05b8270b17ee59dd0eeb791e467ca1
Adriano Brito <adriano.brito@zalando.de>
2023-07-26 16:14:13 +0200

    Add minor improvements on FindValueError

--- src/main/scala/org/apatheia/error/FindValueError.scala
+++ src/main/scala/org/apatheia/error/FindValueError.scala
@@ -1,4 +1,5 @@
 package org.apatheia.error
 
+import org.apatheia.model.Contact
 
-final case class FindValueError(message: String) extends Error
+final case class FindValueError(message: String, contacts: Set[Contact]) extends Error
